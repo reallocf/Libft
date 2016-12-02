e93bb367d91dd707814655e28cda24378bd0271a
Charlie Summers <charliegsummers@gmail.com>
2016-12-01 17:59:08 -0800

    Fix up gnl.

--- get_next_line.c
+++ get_next_line.c
@@ -10,34 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "libft.h"
 
-int		get_next_line(const int fd, char **line)
+char	*copy_until_n(char **s, char *buff, size_t n)
 {
-	static t_buff	*buff[MAX_FD];
-	char			*nl;
+	size_t	s_len;
+	char	*res;
 
-	if (!line || fd < 0 || !(*line = ft_strnew(0)))
-		return (-1);
-	while (1)
-	{
-		if ((!buff[fd] || !buff[fd]->is_s) && !read_into(buff, fd))
-			return (-1);
-		if (!buff[fd]->is_s && !buff[fd]->is_f && clear(buff, fd))
-			return ((**line) ? 1 : 0);
-		if ((nl = ft_strchr(buff[fd]->p, 10)))
-		{
-			if (!(*line = copy_until_n(line, buff[fd]->p, nl - buff[fd]->p)))
-				return (-1);
-			if (!*(buff[fd]->p = nl + 1))
-				buff[fd]->is_s = 0;
-			return (1);
-		}
-		if (!(*line = copy_until_n(line, buff[fd]->p, ft_strlen(buff[fd]->p))))
-			return (-1);
-		if (!(buff[fd]->is_s = 0) && !buff[fd]->is_f)
-			return (1);
-	}
+	if (!*s)
+		s_len = 0;
+	else
+		s_len = ft_strlen(*s);
+	if (!(res = ft_strnew(s_len + n)))
+		return (NULL);
+	if (*s)
+		ft_strcpy(res, *s);
+	ft_strncat(res, buff, n);
+	free(*s);
+	return (res);
 }
 
 t_buff	*read_into(t_buff **buff, int fd)
@@ -69,28 +59,38 @@ t_buff	*read_into(t_buff **buff, int fd)
 	return (buff[fd]);
 }
 
-char	*copy_until_n(char **s, char *buff, size_t n)
-{
-	size_t	s_len;
-	char	*res;
-
-	if (!*s)
-		s_len = 0;
-	else
-		s_len = ft_strlen(*s);
-	if (!(res = ft_strnew(s_len + n)))
-		return (NULL);
-	if (*s)
-		ft_strcpy(res, *s);
-	ft_strncat(res, buff, n);
-	free(*s);
-	return (res);
-}
-
 int		clear(t_buff **buff, int fd)
 {
 	free(buff[fd]->s);
 	buff[fd]->s = NULL;
 	buff[fd]->p = buff[fd]->s;
 	return (1);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static t_buff	*buff[MAX_FD];
+	char			*nl;
+
+	if (!line || fd < 0 || fd >= MAX_FD || !(*line = ft_strnew(0)))
+		return (-1);
+	while (1)
+	{
+		if ((!buff[fd] || !buff[fd]->is_s) && !read_into(buff, fd))
+			return (-1);
+		if (!buff[fd]->is_s && !buff[fd]->is_f && clear(buff, fd))
+			return ((**line) ? 1 : 0);
+		if ((nl = ft_strchr(buff[fd]->p, 10)))
+		{
+			if (!(*line = copy_until_n(line, buff[fd]->p, nl - buff[fd]->p)))
+				return (-1);
+			if (!*(buff[fd]->p = nl + 1))
+				buff[fd]->is_s = 0;
+			return (1);
+		}
+		if (!(*line = copy_until_n(line, buff[fd]->p, ft_strlen(buff[fd]->p))))
+			return (-1);
+		if (!(buff[fd]->is_s = 0) && !buff[fd]->is_f)
+			return (1);
+	}
 }
